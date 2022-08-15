e1e536c080f85c93a11ce403678616a539dc8d91
daydy225 <dabelydadi@gmail.com>
2022-08-15 19:11:14 +0000

    Backend node js notes update

--- index.js
+++ index.js
@@ -27,10 +27,6 @@ let notes = [
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (request, response) => {
-  response.send('<h1>hello World</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
